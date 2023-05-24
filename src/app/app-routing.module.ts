b7a44c3b700e2e2f03a2c5f623fc653252da3a0e
f.soleimani <farid.2014.solaimani@gmail.com>
2023-05-24 17:20:05 +0330

    initial project
    Signed-off-by: f.soleimani <farid.2014.solaimani@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./components/login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: 'signIn',
+    loadChildren: () => import('./components/sign-in/sign-in.module').then(m => m.SignInModule)
+  },
+  {
+    path: '',
+    redirectTo: 'signIn',
+    pathMatch: 'full'
+  },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
